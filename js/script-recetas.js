54f9abe80ca9150706380e2b1e43d2b617246584
Mauro Alori <mauroalori@hotmail.com>
2023-05-10 22:19:34 -0300

    se optimiza la funcionalidad de filtrado mediante las imagenes

--- js/script-recetas.js
+++ js/script-recetas.js
@@ -1,33 +1,29 @@
-function modificarDisplayPlatos(){
-    //id de los checkbox de cada grupo de platos "filtro-platos-xxxxxx"
-    //id de los section de cada grupo de platos  "section-platos-xxxxxx"
-    idPlatosCheckboxArray = this.id.split("-");
-    idPlatosCheckboxArray.splice(0,1,"section");
-    idPlatosSeccion = idPlatosCheckboxArray.join("-");
+function modificarDisplayPlatos() {
+  //id de los checkbox de cada grupo de platos "filtro-platos-xxxxxx"
+  //id de los section de cada grupo de platos  "section-platos-xxxxxx"
+  idPlatosCheckboxArray = this.id.split("-");
+  idPlatosCheckboxArray.splice(0, 1, "section");
+  idPlatosSeccion = idPlatosCheckboxArray.join("-");
 
-    if(this.checked){
-        document.getElementById(idPlatosSeccion).style.display="block";
-    }
-    else{
-        document.getElementById(idPlatosSeccion).style.display="none";
-    }
+  if (this.checked) {
+    document.getElementById(idPlatosSeccion).style.display = "block";
+  } else {
+    document.getElementById(idPlatosSeccion).style.display = "none";
+  }
 }
 
-function load(){
-    const checkboxFiltrado = sessionStorage.getItem("idFiltrado");
-    const checkboxs = document.getElementsByName("filtro-platos");
-    for (const checkbox of checkboxs) {
-        checkbox.addEventListener('change', modificarDisplayPlatos, false);
-        
-        checkbox.checked = true;
-        if(checkboxFiltrado!==null && checkboxFiltrado!=="null" && checkbox.id !== checkboxFiltrado){
-            checkbox.click();
-        }
-        console.log(checkbox.checked);
+function load() {
+  const checkboxFiltrado = sessionStorage.getItem("idFiltrado");
+  const checkboxs = document.getElementsByName("filtro-platos");
+  for (const checkbox of checkboxs) {
+    checkbox.addEventListener("change", modificarDisplayPlatos, false);
+
+    checkbox.checked = true;
+    if (checkboxFiltrado !== null && checkbox.id !== checkboxFiltrado) {
+      checkbox.click();
     }
-    sessionStorage.setItem("idFiltrado",null);
-    console.log(sessionStorage.getItem("idFiltrado"))
-    
+  }
+  sessionStorage.removeItem("idFiltrado");
 }
 
 document.addEventListener("DOMContentLoaded", load, false);
